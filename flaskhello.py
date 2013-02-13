136c5b4b5fc83b85efa681e5f9822b1ea95194e4
Aria Moraine <ariamoraine@gmail.com>
2013-02-13 14:36:54 -0500

    setting up basic flask app

---
+++ flaskhello.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import socket
+app = Flask(__name__)
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+	return render_template("hello.html", name=name)
+
+@app.route("/aria")
+def aria():
+	return "My name is Aria."
+
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    app.run(port=port)
