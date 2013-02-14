2f0b9b342a72f3a80daffd355a9828201f014fd6
Aria Moraine <ariamoraine@gmail.com>
2013-02-13 20:49:26 -0500

    making kitten generator

--- flaskhello.py
+++ flaskhello.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 import socket
+
 app = Flask(__name__)
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
+@app.route("/")
+@app.route("/<name>")
 def hello(name=None):
 	return render_template("hello.html", name=name)
 
@@ -12,8 +13,8 @@ def aria():
 	return "My name is Aria."
 
 if __name__ == "__main__":
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('localhost', 0))
-    port = sock.getsockname()[1]
-    sock.close()
-    app.run(port=port)
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.bind(('localhost', 0))
+	port = sock.getsockname()[1]
+	sock.close()
+	app.run(port=port)
